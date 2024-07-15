e7231c038c176f34e58d7a8c362ca207e57add02
Alexander William <alexwilliam112@gmail.com>
2024-07-15 22:30:13 +0700

    feat: Added endpoint for dashboardTable

--- analytics-service/controllers/Controller.js
+++ analytics-service/controllers/Controller.js
@@ -6,6 +6,7 @@ const {
   OverallConformance_PieChart,
   AverageConformanceByProcess_LineChart,
   TopTenNonConformTable,
+  DashboardTable
 } = require("../queries/index");
 
 class Controller {
@@ -55,58 +56,37 @@ class Controller {
         CompanyId: parseFloat(task.CompanyId),
       }));
 
-      const taskRecords = await prisma.task.createMany({
+      await prisma.task.createMany({
         data: tasks,
       });
 
       res.status(201).json({
         statusCode: 201,
-        message: "Tasks created successfully",
-        data: taskRecords,
       });
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }
 
   static async getTableData(req, res, next) {
     try {
-      const { startDate, endDate, process, department, person } = req.query;
+      const {
+        query,
+        parameters,
+      } = DashboardTable(req.query);
 
-      const data = await prisma.$queryRaw`
-        SELECT
-          t."eventName",
-          CAST(e."benchmarkTime" AS INTEGER) AS "benchmarkTime",
-          AVG(CAST(t."time" AS FLOAT)) AS average_actual,
-          CAST(e."ProcessId" AS INTEGER) AS "ProcessId",
-          CAST(
-            (
-              (
-                COUNT(t."time") - SUM(
-                  CASE
-                    WHEN CAST(t."time" AS FLOAT) > CAST(e."benchmarkTime" AS FLOAT) THEN 1
-                    ELSE 0
-                  END
-                )
-              ) * 1.0 / COUNT(t."time")
-            ) AS FLOAT
-          ) AS conformance_rate,
-          CAST(COUNT(t."eventName") AS INTEGER) AS total_case
-        FROM
-          "Task" t
-          LEFT JOIN "Event" e ON t."eventName" = e."eventName"
-        WHERE
-          t."timestamp" BETWEEN '2020-01-01' AND '2024-12-31'
-          AND t."department" = 'Marketing Emporio Armani 7'
-          AND t."name" = 'Jane Doe'
-          AND t."processName" = 'PURCHASE_ORDER'
-        GROUP BY
-          t."eventName",
-          e."benchmarkTime",
-          e."ProcessId";
-        `;
+      const tableData_raw = await prisma.$queryRawUnsafe(
+        query,
+        ...parameters
+      );
+      
+      const tableData = tableData_raw.map((row) => {
+        row.conformance_rate = row.conformance_rate * 100
+        return row
+      })
 
-      res.send(data);
+      res.status(200).json(tableData)
     } catch (error) {
       next(error);
     }
