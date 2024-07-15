d33d893bb72b43667b9e7387197e94b57afc9665
Yobel Sahala <34935946+aGlassofMarjan@users.noreply.github.com>
2024-07-15 23:17:08 +0700

    dashboard filter

--- client/src/app/dashboard/page.jsx
+++ client/src/app/dashboard/page.jsx
@@ -69,129 +69,142 @@ const Dashboard = () => {
         <div className="flex flex-col sm:gap-4 sm:py-4">
           <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
             <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-              <form className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-6">Form</h2>
+              <div className="bg-white p-8 rounded-lg shadow-md w-full">
+                <h2 className="text-2xl font-light mb-6">Filter</h2>
+                <form className="lg:flex gap-5 items-center">
+                  <div>
+                    <label
+                      htmlFor="department"
+                      className="form-control w-auto max-w-xs lg:w-48"
+                    >
+                      <div className="label">
+                        <span className="label-text">Department</span>
+                      </div>
+                      <select
+                        id="department"
+                        name="department"
+                        value={department}
+                        onChange={(e) => setDepartment(e.target.value)}
+                        className="select select-bordered"
+                      >
+                        <option value="" disabled selected>
+                          Select option
+                        </option>
+                        {resultDepartments.map((el, idx) => (
+                          <option key={idx} value="">
+                            {el}
+                          </option>
+                        ))}
+                      </select>
+                    </label>
+                  </div>
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="department"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Department
-                  </label>
-                  <select
-                    id="department"
-                    name="department"
-                    value={department}
-                    onChange={(e) => setDepartment(e.target.value)}
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  >
-                    <option value="" disabled selected>
-                      Select option
-                    </option>
-                    {resultDepartments.map((el, idx) => (
-                      <option key={idx} value="">
-                        {el}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+                  <div>
+                    <label
+                      htmlFor="person"
+                      className="form-control w-auto max-w-xs lg:w-48"
+                    >
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="person"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Person
-                  </label>
-                  <select
-                    id="person"
-                    name="person"
-                    value={person}
-                    onChange={(e) => setPerson(e.target.value)}
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  >
-                    <option value="" disabled selected>
-                      Select option
-                    </option>
-                    {resultPerson.map((el, idx) => (
-                      <option key={idx} value="">
-                        {el}
-                      </option>
-                    ))}
-                    {/* Add options here */}
-                  </select>
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="process"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Process
-                  </label>
-                  <select
-                    id="process"
-                    name="process"
-                    value={process}
-                    onChange={(e) => setProcess(e.target.value)}
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  >
-                    <option value="" disabled selected>
-                      Select option
-                    </option>
-                    {resultProcesses.map((el, idx) => (
-                      <option key={idx} value="">
-                        {el}
-                      </option>
-                    ))}
-                    {/* Add options here */}
-                  </select>
-                </div>
+                      <div className="label">
+                        <span className="label-text">Person</span>
+                      </div>
+                      <select
+                        id="person"
+                        name="person"
+                        value={person}
+                        onChange={(e) => setPerson(e.target.value)}
+                        className="select select-bordered"
+                      >
+                        <option value="" disabled selected>
+                          Select option
+                        </option>
+                        {resultPerson.map((el, idx) => (
+                          <option key={idx} value="">
+                            {el}
+                          </option>
+                        ))}
+                        {/* Add options here */}
+                      </select>
+                    </label>
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="process"
+                      className="form-control w-auto max-w-xs lg:w-48"
+                    >
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="startDate"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Start Date
-                  </label>
-                  <input
-                    type="date"
-                    id="startDate"
-                    name="startDate"
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                </div>
+                      <div className="label">
+                        <span className="label-text">Process</span>
+                      </div>
+                      <select
+                        id="process"
+                        name="process"
+                        value={process}
+                        onChange={(e) => setProcess(e.target.value)}
+                        className="select select-bordered"
+                      >
+                        <option value="" disabled selected>
+                          Select option
+                        </option>
+                        {resultProcesses.map((el, idx) => (
+                          <option key={idx} value="">
+                            {el}
+                          </option>
+                        ))}
+                        {/* Add options here */}
+                      </select>
+                    </label>
+                  </div>
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="endDate"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    End Date
-                  </label>
-                  <input
-                    type="date"
-                    id="endDate"
-                    name="endDate"
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                    className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-                  />
-                </div>
+                  <div>
+                    <label
+                      htmlFor="startDate"
+                      className="form-control w-full max-w-xs"
+                    >
+                      <div className="label">
+                        <span className="label-text">Start Date</span>
+                      </div>
+                      <input
+                        type="date"
+                        id="startDate"
+                        name="startDate"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                        className="input input-bordered w-full max-w-xs"
+                      />
+                    </label>
+                  </div>
 
-                <div className="flex justify-end">
+                  <div>
+                    <label
+                      htmlFor="endDate"
+                      className="form-control w-full max-w-xs"
+                    >
+                      <div className="label">
+                        <span className="label-text">End Date</span>
+                      </div>
+                      <input
+                        type="date"
+                        id="endDate"
+                        name="endDate"
+                        value={endDate}
+                        onChange={(e) => setEndDate(e.target.value)}
+                        className="input input-bordered w-full max-w-xs"
+                      />
+                    </label>
+                  </div>
+
+
+                </form>
+                <div className="flex justify-end px-5 mt-5">
                   <button
                     type="submit"
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    className="btn btn-[#6E8672] "
                   >
                     Submit
                   </button>
                 </div>
-              </form>
-
+              </div>
               <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
                 <div className="sm:col-span-2 p-6 bg-[#F1F2F2] border-2 border-[#6E8672] rounded-lg shadow">
                   <div className="pb-6 flex justify-between">
